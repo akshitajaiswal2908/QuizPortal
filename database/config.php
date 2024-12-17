9826c857b6c61c3fa573a18161e2774ec45b9ed2
akshitajaiswal2908 <akshitajaiswal2908@gmail.com>
2024-12-18 01:25:21 +0530

    complete project

---
+++ database/config.php
@@ -0,0 +1,9 @@
+<?php
+//database configurations
+define("DB_HOST","localhost");
+define("DB_UNAME","root");
+define("DB_PASS","");
+define("DB_DNAME","quizz");
+$conn=mysqli_connect(DB_HOST,DB_UNAME,DB_PASS,DB_DNAME);
+?>
+
